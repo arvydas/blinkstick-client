cee4f8092382b72f297fcdb5d0fa40e261a26a6d
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2014-10-08 17:11:35 +0100

    Added extension methods to get Gtk color values in the range of 0..1

---
+++ BlinkStick/Utils/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gdk;
+
+namespace BlinkStickClient.Utils
+{
+    public static class GdkColorExcensions
+    {
+        public static double GetRed(this Color color) 
+        {
+            return (1.0 * color.Red / 0x10000);
+        }
+
+        public static double GetGreen(this Color color) 
+        {
+            return (1.0 * color.Green / 0x10000);
+        }
+
+        public static double GetBlue(this Color color) 
+        {
+            return (1.0 * color.Blue / 0x10000);
+        }
+    }
+
+}
+
