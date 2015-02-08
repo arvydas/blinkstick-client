8afa19338b48defceb7a7290dd0d9edb19b54bba
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2015-02-08 22:12:43 +0000

    Extension method for DateTime to display time ago

--- BlinkStick/Classes/ExtensionMethods.cs
+++ BlinkStick/Classes/ExtensionMethods.cs
@@ -18,6 +18,41 @@ namespace BlinkStickClient
 
             return new Gdk.Color(d, d, d);
         }
+
+        public static string TimeAgo(this DateTime dt)
+        {
+            TimeSpan span = DateTime.Now - dt;
+            if (span.Days > 365)
+            {
+                int years = (span.Days / 365);
+                if (span.Days % 365 != 0)
+                    years += 1;
+                return String.Format("~{0} {1} ago", 
+                    years, years == 1 ? "year" : "years");
+            }
+            if (span.Days > 30)
+            {
+                int months = (span.Days / 30);
+                if (span.Days % 31 != 0)
+                    months += 1;
+                return String.Format("~{0} {1} ago", 
+                    months, months == 1 ? "month" : "months");
+            }
+            if (span.Days > 0)
+                return String.Format("~{0} {1} ago", 
+                    span.Days, span.Days == 1 ? "day" : "days");
+            if (span.Hours > 0)
+                return String.Format("~{0} {1} ago", 
+                    span.Hours, span.Hours == 1 ? "hour" : "hours");
+            if (span.Minutes > 0)
+                return String.Format("~{0} {1} ago", 
+                    span.Minutes, span.Minutes == 1 ? "minute" : "minutes");
+            if (span.Seconds > 5)
+                return "<1 minute ago";
+            if (span.Seconds <= 5)
+                return "just now";
+            return string.Empty;
+        }
     }
 }
 
