7df1f5a83e03e108156f4ab9b33a1a2e6b4b96b4
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2015-02-11 20:13:47 +0000

    Implemented RAM notification

--- BlinkStick/DataModel/NotificationRam.cs
+++ BlinkStick/DataModel/NotificationRam.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 namespace BlinkStickClient.DataModel
 {
-    public class NotificationRam : PatternNotification
+    public class NotificationRam : HardwareNotification
     {
         public override string GetTypeName()
         {
@@ -11,6 +14,7 @@ namespace BlinkStickClient.DataModel
 
         public NotificationRam()
         {
+            UsesPerformanceCounter = false;
         }
 
         public override Notification Copy(Notification notification)
@@ -21,6 +25,86 @@ namespace BlinkStickClient.DataModel
             }
 
             return base.Copy(notification);
+        }
+
+        public override bool IsSupported()
+        {
+            return HidSharp.PlatformDetector.RunningPlatform() == HidSharp.PlatformDetector.Platform.Windows;
+        }
+
+        public override Gtk.Widget GetEditorWidget()
+        {
+            return new CpuEditorWidget();
+        }
+
+        protected override void InitializePerformanceCounter (object sender, DoWorkEventArgs e)
+        {
+            //Not using here
+        }
+
+        #region implemented abstract members of HardwareNotification
+
+        protected override int GetValue()
+        {
+            Int64 phav = GetPhysicalAvailableMemoryInMiB();
+            Int64 tot = GetTotalMemoryInMiB();
+            decimal percentFree = ((decimal)phav / (decimal)tot) * 100;
+            decimal percentOccupied = 100 - percentFree;
+
+            return (int)percentOccupied;
+        }
+
+        #endregion
+
+        [DllImport("psapi.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetPerformanceInfo([Out] out PerformanceInformation PerformanceInformation, [In] int Size);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PerformanceInformation
+        {
+            public int Size;
+            public IntPtr CommitTotal;
+            public IntPtr CommitLimit;
+            public IntPtr CommitPeak;
+            public IntPtr PhysicalTotal;
+            public IntPtr PhysicalAvailable;
+            public IntPtr SystemCache;
+            public IntPtr KernelTotal;
+            public IntPtr KernelPaged;
+            public IntPtr KernelNonPaged;
+            public IntPtr PageSize;
+            public int HandlesCount;
+            public int ProcessCount;
+            public int ThreadCount;
+        }
+
+        public static Int64 GetPhysicalAvailableMemoryInMiB()
+        {
+            PerformanceInformation pi = new PerformanceInformation();
+            if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+            {
+                return Convert.ToInt64((pi.PhysicalAvailable.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+
+        public static Int64 GetTotalMemoryInMiB()
+        {
+            PerformanceInformation pi = new PerformanceInformation();
+            if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+            {
+                return Convert.ToInt64((pi.PhysicalTotal.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+            }
+            else
+            {
+                return -1;
+            }
+
         }
     }
 }
