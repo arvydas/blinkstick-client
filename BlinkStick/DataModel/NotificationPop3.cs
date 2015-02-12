567e7cdddab51ac0ec24981cd7448c1d5142363d
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2015-02-12 22:27:27 +0000

    Implemented POP3 notification

--- BlinkStick/DataModel/NotificationPop3.cs
+++ BlinkStick/DataModel/NotificationPop3.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using OpenPop.Pop3;
 
 namespace BlinkStickClient.DataModel
 {
-    public class NotificationPop3 : PatternNotification
+    public class NotificationPop3 : EmailNotification
     {
         public override string GetTypeName()
         {
@@ -21,6 +22,35 @@ namespace BlinkStickClient.DataModel
             }
 
             return base.Copy(notification);
+        }
+
+        public override Gtk.Widget GetEditorWidget()
+        {
+            EmailEditorWidget widget = new EmailEditorWidget();
+            widget.DefaultPort = 110;
+            widget.DefaultSslPort = 995;
+            return widget;
+        }
+
+        public override bool IsSupported()
+        {
+            return true;
+        }
+
+        public override int GetValue(String username, String password, String server, int port, Boolean useSsl)
+        {
+            // The client disconnects from the server when being disposed
+            using(Pop3Client client = new Pop3Client())
+            {
+                // Connect to the server
+                client.Connect(server, port, useSsl);
+
+                // Authenticate ourselves towards the server
+                client.Authenticate(username, password);
+
+                // Get the number of messages in the inbox
+                return client.GetMessageCount();
+            }
         }
     }
 }
