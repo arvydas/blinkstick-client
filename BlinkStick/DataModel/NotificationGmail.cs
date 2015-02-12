e7f6649f4baee638b87ee522a77e90e905d6d7db
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2015-02-12 14:15:41 +0000

    Implemented GMail notification

--- BlinkStick/DataModel/NotificationGmail.cs
+++ BlinkStick/DataModel/NotificationGmail.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Net;
+using System.Net.Security;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BlinkStickClient.DataModel
 {
     public class NotificationGmail : PatternNotification
     {
+        public String Email { get; set; }
+
+        public String Password { get; set; }
+
+        public int CheckPeriod { set; get; }
+
+        private uint RefreshTimer;
+
         public override string GetTypeName()
         {
             return "GMail";
@@ -11,6 +25,9 @@ namespace BlinkStickClient.DataModel
 
         public NotificationGmail()
         {
+            this.Email = "";
+            this.Password = "";
+            this.CheckPeriod = 1;
         }
 
         public override Notification Copy(Notification notification)
@@ -20,7 +37,149 @@ namespace BlinkStickClient.DataModel
                 notification = new NotificationGmail();
             }
 
+            ((NotificationGmail)notification).Email = this.Email;
+            ((NotificationGmail)notification).Password = this.Password;
+            ((NotificationGmail)notification).CheckPeriod = this.CheckPeriod;
+
             return base.Copy(notification);
+        }
+
+        public override bool IsSupported()
+        {
+            return true;
+        }
+
+        public override Gtk.Widget GetEditorWidget()
+        {
+            return new GmailEditorWidget();
+        }
+
+        public override void Start()
+        {
+            if (Running)
+                return;
+
+            log.InfoFormat("Starting {0} monitoring", GetTypeName());
+
+            base.Start();
+
+            #if DEBUG
+            //Faster checks for debugging
+            int timeFrame = 10000;
+            #else
+            int timeFrame = 60000;
+            #endif
+            log.DebugFormat("Starting timer at {0} ms", (uint)(this.CheckPeriod * timeFrame));
+            RefreshTimer = GLib.Timeout.Add((uint)(this.CheckPeriod * timeFrame), new GLib.TimeoutHandler(CheckUsage));
+
+            log.DebugFormat("{0} monitoring started", GetTypeName());
+        }
+
+        public override void Stop()
+        {
+            if (!Running)
+                return;
+            log.InfoFormat("Stopping {0} monitoring", GetTypeName());
+
+            //Stop timer
+            GLib.Source.Remove(RefreshTimer);
+
+            base.Stop();
+            log.DebugFormat("{0} monitoring stopped", GetTypeName());
+        }
+
+        public override bool RequiresMonitoring()
+        {
+            return true;
+        }
+
+        protected bool CheckUsage()
+        {
+            int unreadEmails = 0;
+
+            try
+            {
+                unreadEmails = GetValue(this.Email, this.Password);
+            }
+            catch (Exception e)
+            {
+                log.Error(e.Message);
+                return true;
+            }
+
+            log.DebugFormat("Unread emails: {0}", unreadEmails);
+
+            if (unreadEmails > 0)
+            {
+                OnTriggered(String.Format("Unread emails: {0}", unreadEmails));
+            }
+
+            return true;
+        }
+
+        public override void Dispose()
+        {
+            Stop();
+            base.Dispose();
+        }
+
+        public int GetValue(String email, String password)
+        {
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback = Validator;
+                WebRequest webGmailRequest = WebRequest.Create (@"https://mail.google.com/mail/feed/atom");
+                webGmailRequest.PreAuthenticate = true;
+
+                NetworkCredential loginCredentials = new NetworkCredential (email, password);
+                webGmailRequest.Credentials = loginCredentials;
+
+                WebResponse webGmailResponse = webGmailRequest.GetResponse ();
+                Stream strmUnreadMailInfo = webGmailResponse.GetResponseStream ();
+
+                StringBuilder sbUnreadMailInfo = new StringBuilder ();
+                byte[] buffer = new byte[8192];
+                int byteCount = 0;
+
+                while ((byteCount = strmUnreadMailInfo.Read(buffer, 0, buffer.Length)) > 0)
+                    sbUnreadMailInfo.Append (System.Text.Encoding.ASCII.GetString (buffer, 0, byteCount));
+
+                XmlDocument UnreadMailXmlDoc = new XmlDocument ();
+                UnreadMailXmlDoc.LoadXml (sbUnreadMailInfo.ToString ());
+                XmlNodeList UnreadMailEntries = UnreadMailXmlDoc.GetElementsByTagName ("entry");
+
+                return UnreadMailEntries.Count;
+            }
+            catch (WebException e)
+            {
+                if (e.Status == WebExceptionStatus.ProtocolError)
+                {
+                    var response = e.Response as HttpWebResponse;
+
+                    if (response != null && response.StatusCode == HttpStatusCode.Unauthorized)
+                    {
+                        throw new Exception("Invalid username or password");
+                    }
+                    else
+                    {
+                        throw e;
+                    }
+                }
+                else
+                {
+                    throw e;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static bool Validator (object sender, X509Certificate certificate, X509Chain chain, 
+            SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
         }
     }
 }
---
+++ BlinkStick/Editors/GmailEditorWidget.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using BlinkStickClient.DataModel;
+using BlinkStickClient.Utils;
+
+namespace BlinkStickClient
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class GmailEditorWidget : Gtk.Bin, IEditorInterface
+    {
+        private NotificationGmail Notification;
+
+        public GmailEditorWidget()
+        {
+            this.Build();
+        }
+
+        #region IEditorInterface implementation
+
+        public void SetNotification(BlinkStickClient.DataModel.Notification notification)
+        {
+            Notification = (notification as NotificationGmail);
+
+            entryEmail.Text = Notification.Email;
+            entryPassword.Text = Notification.Password;
+            spinbuttonCheckPeriod.Value = Notification.CheckPeriod;
+        }
+
+        public bool IsValid(Gtk.Window window)
+        {
+            if (this.entryEmail.Text.Trim() == "")
+            {
+                MessageBox.Show(window, "Please enter Email", Gtk.MessageType.Error);
+                return false;
+            }
+
+            if (this.entryPassword.Text.Trim() == "")
+            {
+                MessageBox.Show(window, "Please enter password", Gtk.MessageType.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        public void UpdateNotification()
+        {
+            Notification.Email = entryEmail.Text;
+            Notification.Password = entryPassword.Text;
+            Notification.CheckPeriod = spinbuttonCheckPeriod.ValueAsInt;
+        }
+
+        #endregion
+
+        protected void OnButtonRefreshClicked (object sender, EventArgs e)
+        {
+            if (this.entryEmail.Text.Trim() == "")
+            {
+                labelCurrentValue.Text = "Value: (Email missing)";
+                return;
+            }
+
+            if (this.entryPassword.Text.Trim() == "")
+            {
+                labelCurrentValue.Text = "Value: (Password missing)";
+                return;
+            }
+
+            try
+            {
+                labelCurrentValue.Text = String.Format("Value: {0} unread", this.Notification.GetValue(this.entryEmail.Text, this.entryPassword.Text));
+            }
+            catch (Exception ex)
+            {
+                labelCurrentValue.Text = String.Format("Value: {0}", ex.Message);
+            }
+        }
+    }
+}
+
