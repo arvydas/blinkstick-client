bec7e4adb8bbbc6d7954b850a6b8b9ab223974f2
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2015-04-23 19:15:42 +0100

    Release 2.0-beta1

--- BlinkStickClient/AssemblyInfo.cs
+++ BlinkStickClient/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 #if DEBUG
 [assembly: AssemblyConfiguration("debug")]
 #else
-[assembly: AssemblyConfiguration("beta2")] //Set beta1, beta2, beta3 and etc. Leave empty for release.
+[assembly: AssemblyConfiguration("beta1")] //Set beta1, beta2, beta3 and etc. Leave empty for release.
 #endif
 [assembly: AssemblyCompany("Agile Innovative Ltd")]
 [assembly: AssemblyProduct("BlinkStick")]
@@ -21,7 +21,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.3.*")]
+[assembly: AssemblyVersion("2.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
