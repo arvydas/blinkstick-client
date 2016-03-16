b9de43563b582edb3b6daf348310c636702b5bad
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2016-03-16 13:02:24 +0000

    Added icon generator code

---
+++ util/icon-generator/README.txt
@@ -0,0 +1,7 @@
+Run in sequence:
+
+pip install icon_font_to_png
+
+python generate.py --download font-awesome
+
+generate.bat
---
+++ util/icon-generator/generate.bat
@@ -0,0 +1,7 @@
+python generate.py --css font-awesome.css --ttf fontawesome-webfont.ttf --size 32 --filename icon-dark- usb bell th calendar cog question-circle sign-out 
+
+python generate.py --css font-awesome.css --ttf fontawesome-webfont.ttf --size 16 --filename icon-dark- plus refresh trash pencil-square-o clone clipboard play stop arrow-up arrow-down check-square-o square-o
+
+python generate.py --css font-awesome.css --ttf fontawesome-webfont.ttf --size 16 --filename icon-dark-cog-small cog
+
+python generate.py --css font-awesome.css --ttf fontawesome-webfont.ttf --size 16 --filename icon-dark-usb-small usb
