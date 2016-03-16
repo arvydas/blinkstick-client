b0a344b2f2ffcc2987a5cbd02d4eb482c49ba747
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2016-03-16 18:27:02 +0000

    Implemented battery notification for OS X

--- BlinkStickClient.Base/DataModel/NotificationBattery.cs
+++ BlinkStickClient.Base/DataModel/NotificationBattery.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows.Forms;
+using System.Diagnostics;
 using log4net;
 
 namespace BlinkStickClient.DataModel
@@ -29,30 +30,76 @@ namespace BlinkStickClient.DataModel
 
         public override bool IsSupported()
         {
-            return HidSharp.PlatformDetector.RunningPlatform() == HidSharp.PlatformDetector.Platform.Windows;
+            return HidSharp.PlatformDetector.RunningPlatform() == HidSharp.PlatformDetector.Platform.Windows ||
+				HidSharp.PlatformDetector.RunningPlatform() == HidSharp.PlatformDetector.Platform.Mac;
         }
 
         #region implemented abstract members of HardwareNotification
 
         public override int GetValue()
         {
-            if (SystemInformation.PowerStatus.BatteryChargeStatus == BatteryChargeStatus.NoSystemBattery)
-            {
-                throw new Exception("No battery");
-            }
+			if (HidSharp.PlatformDetector.RunningPlatform() == HidSharp.PlatformDetector.Platform.Windows)
+			{
+				if (SystemInformation.PowerStatus.BatteryChargeStatus == BatteryChargeStatus.NoSystemBattery)
+				{
+					throw new Exception("No battery");
+				}
 
-            return (int)(SystemInformation.PowerStatus.BatteryLifePercent * 100);
+				return (int)(SystemInformation.PowerStatus.BatteryLifePercent * 100);
+			}
+			else if (HidSharp.PlatformDetector.RunningPlatform() == HidSharp.PlatformDetector.Platform.Mac)
+			{
+				var psi = new ProcessStartInfo(global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "scripts", "osx-batt.sh"))
+				{
+					RedirectStandardOutput = true,
+					UseShellExecute = false
+				};
+				Process p = Process.Start(psi);
+				string outString = p.StandardOutput.ReadToEnd();
+				p.WaitForExit();
+
+				if (outString.Trim() == "")
+				{
+					throw new Exception("No battery or battery info could not be read");
+				}
+
+				try
+				{
+					return (int)Math.Round(Convert.ToDouble(outString.Trim ()));
+				}
+				catch (Exception e) {
+					log.ErrorFormat ("Failed to convert string \"{0}\" to int: {1}", outString, e);
+					return 0;
+				}
+			}
+
+			return 0;
         }
 
         #endregion
 
         public override void Start()
         {
-            if (SystemInformation.PowerStatus.BatteryChargeStatus == BatteryChargeStatus.NoSystemBattery)
-            {
-                log.Info("Unable to start monitoring, no battery detected"); 
-                return;
-            }
+			if (HidSharp.PlatformDetector.RunningPlatform() == HidSharp.PlatformDetector.Platform.Windows)
+			{
+				if (SystemInformation.PowerStatus.BatteryChargeStatus == BatteryChargeStatus.NoSystemBattery)
+	            {
+	                log.Info("Unable to start monitoring, no battery detected"); 
+	                return;
+	            }
+			}
+			else if (HidSharp.PlatformDetector.RunningPlatform() == HidSharp.PlatformDetector.Platform.Mac)
+			{
+				try
+				{
+					GetValue();
+				}
+				catch (Exception e)
+				{
+					log.InfoFormat("Unable to start monitoring: {0}", e.Message);
+					return;
+				}
+			}
 
             base.Start();
         }
