096939b5e1d2eb20a96b0b24f18b7eed18256391
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2016-03-18 13:02:42 +0000

    OSX installer implementation

--- Install/build-osx.sh
+++ Install/build-osx.sh
@@ -1,30 +1,59 @@
 #!/bin/sh
 
-APP_NAME=BlinkStickClient.app
+APP_NAME="BlinkStick Client.app"
+BUILD=Release
+BINARY=../BlinkStickClient/bin/$BUILD
 
-SOURCE=./osx
+mono $BINARY/BlinkStickClient.exe --build-config version.iss
 
-mkdir -p setup
-mkdir -p build
+VERSION_STRING=`sed -n 's/.*AppVersion \"\(.*\)\".*/\1/p' version.iss`
+VERSION=`sed -n 's/.*AppFullVersion \"\([0-9]*\.[0-9]*\.[0-9]*\).*\".*/\1/p' version.iss`
 
-rm -rf build/$APP_NAME
+OUTPUT=./setup/BlinkStickClient-$VERSION_STRING.pkg
 
+RED='\x1b[31m'
+GREEN='\x1b[32m'
+NC='\x1b[0m'
 
-mkdir -p build/$APP_NAME
-mkdir -p build/$APP_NAME/Contents
-mkdir -p build/$APP_NAME/Contents/MacOS
-mkdir -p build/$APP_NAME/Contents/Resources
+echo "Building $RED$VERSION_STRING$NC ($VERSION)...\r\n"
 
-cp -a $SOURCE/. build/$APP_NAME/Contents
-cp ../BlinkStickClient/bin/Release/*.dll build/$APP_NAME/Contents/MacOS
-cp ../BlinkStickClient/bin/Release/*.exe build/$APP_NAME/Contents/MacOS
-cp ../BlinkStickClient/bin/Release/*.config build/$APP_NAME/Contents/MacOS
-cp ../BlinkStickClient/bin/Release/*.ico build/$APP_NAME/Contents/MacOS
-cp ../BlinkStickClient/bin/Release/*.png build/$APP_NAME/Contents/MacOS
-cp -a theme build/$APP_NAME/Contents/MacOS/Theme
-cp -a ../scripts build/$APP_NAME/Contents/MacOS/scripts
+mkdir -p ./setup
+mkdir -p ./build
 
-#pkgbuild --analyze --root ./build/BlinkStickClient.app BlinkStickClient.plist
-#pkgbuild --identifier com.aginno.BlinkStickClient --root ./build/BlinkStickClient.app --install-location /Applications blinkstick-client.pkg
+printf "Clean up if left over build exists..."
+rm -rf "./build/$APP_NAME"
+printf "$GREEN Done$NC\n"
 
+printf "Prepare all directories..."
+mkdir -p "./build/$APP_NAME"
+mkdir -p "./build/$APP_NAME/Contents"
+mkdir -p "./build/$APP_NAME/Contents/MacOS"
+mkdir -p "./build/$APP_NAME/Contents/Resources"
+printf "$GREEN Done$NC\n"
 
+printf "Copy contents..."
+cp -a ./osx/. "./build/$APP_NAME/Contents"
+cp $BINARY/*.dll "./build/$APP_NAME/Contents/MacOS"
+cp $BINARY/*.exe "./build/$APP_NAME/Contents/MacOS"
+cp $BINARY/*.config "./build/$APP_NAME/Contents/MacOS"
+cp $BINARY/*.ico "./build/$APP_NAME/Contents/MacOS"
+cp $BINARY/*.png "./build/$APP_NAME/Contents/MacOS"
+cp -a theme "./build/$APP_NAME/Contents/MacOS/Theme"
+cp -a ../scripts "./build/$APP_NAME/Contents/MacOS/scripts"
+printf "$GREEN Done$NC\n"
+
+printf "Updating version info..."
+sed -i '' -e "s/{VERSION}/$VERSION/g" "./build/$APP_NAME/Contents/Info.plist"
+sed -i '' -e "s/{VERSION_STRING}/$VERSION_STRING/g" "./build/$APP_NAME/Contents/Info.plist"
+printf "$GREEN Done$NC\n"
+
+printf "Building PKG..."
+pkgbuild --quiet --identifier com.agileinnovative.BlinkStickClient --root ./build --install-location /Applications $OUTPUT
+printf "$GREEN Done$NC\n"
+
+printf "Clean up..."
+rm -rf "build/$APP_NAME"
+printf "$GREEN Done$NC\n"
+
+echo "\r\n\r\nBuild complete!"
+echo "    $GREEN$OUTPUT$NC"
